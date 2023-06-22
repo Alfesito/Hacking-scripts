fc488f28c954abd819bf6ea55b7374b21a85e3c5
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-06-22 11:23:42 +0200

    recon.sh

--- recon.sh
+++ recon.sh
@@ -15,13 +15,38 @@ scanyenum() {
     openPorts=$(grep -oP '\d+(?=/open/tcp)' portscan | tr '\n' ',' | sed 's/,$//')
     sudo nmap -sV -sC -oN versionPorts -p"$openPorts" "$ip"
 
-    cd enum
+    cd ../enum
     if [[ $openPorts == *'80'* ]]; then
+        echo -e "\HTTP!"
+        whatweb "http://$ip"
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpEnum -u "http://$ip/FUZZ"
         whatweb "http://$ip" >> httpEnum
     elif [[ $openPorts == *'443'* ]]; then
+        echo -e "\HTTPS!"
+        whatweb "https://$ip"
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpsEnum -u "https://$ip/FUZZ"
         whatweb "https://$ip" >> httpsEnum
+    elif [[ $openPorts == *'21'* ]]; then
+        echo -e "\FTP!"
+        nmap  -n -Pn -oN ftpScript --script ftp-* -p 21 "$ip"
+    elif [[ $openPorts == *'22'* ]]; then
+        echo -e "\SSH!"
+        nmap  -n -Pn -oN sshScript --script ssh-* -p 22 "$ip"
+    elif [[ $openPorts == *'23'* ]]; then
+        echo -e "\TelNet!"
+        nmap -n -Pn -oN telnetScript --script "*telnet* and safe" -p 23 "$ip"
+    elif [[ $openPorts == *'25'* || $openPorts == *'465'* || $openPorts == *'587'*]]; then
+        echo -e "\SMTP!"
+        nmap -p25,465,587 -oN smtpScript --script smtp-* "$ip"
+        #Perform enumeration actions
+    elif [[ $openPorts == *'53'* ]]; then
+        echo -e "\DNS!"
+        nmap -n -oN dnsScript --script "(default and *dns*) or fcrdns or dns-srv-enum or dns-random-txid or dns-random-srcport" "$ip"
+    elif [[ $openPorts == *'137'* || $openPorts == *'138'* || $openPorts == *'139'*]]; then
+        echo -e "\NetBios!"
+        nmblookup -A "$ip"
+        nbtscan "$ip"/30
+        sudo nmap -sU -sV -T4 -oN netbiosScript --script nbstat.nse -p137 -Pn -n "$ip"
     fi
 
     cd ..
