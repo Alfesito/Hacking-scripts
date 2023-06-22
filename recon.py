60463a3b6c2ec6f0adde8c5986358af08b9e5ff3
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-06-22 10:41:09 +0200

    recon.sh

--- recon.py
+++ recon.py
@@ -5,17 +5,17 @@ import subprocess
 from subprocess import STDOUT
 import os
 
-# name = str(sys.argv[1])
-ip = str(sys.argv[1])
+name = str(sys.argv[1])
+ip = str(sys.argv[2])
 
 def dir():
-    # os.system('mkdir '+name+' && cd '+name)
+    os.system('mkdir '+name+' && cd '+name)
     os.system('mkdir nmap && mkdir exploits && mkdir doc && mkdir enum')
     scanyenum()
 
 def scanyenum():
     os.system('cd nmap')
-    os.system('sudo nmap -sS -n -Pn -min-rate 5000 -p- --open -oX portscan'+ip)
+    os.system('sudo nmap -sS -n -Pn -min-rate 5000 -p- --open -oG portscan'+ip)
     salida = subprocess.check_output("grep -oP '\d+(?=/open/tcp)' portscan | tr '\n' ',' | sed 's/,$//'", shell=True, text=True)
     openPorts = salida.strip()
     os.system('sudo nmap -sV -sC -oN versionPorts -p '+openPorts+' '+ip)
@@ -27,7 +27,7 @@ def scanyenum():
         os.system('whatweb http://'+ip+ '>> httpEnum')
     elif '443' in openPorts.split(','):
         os.system('ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpsEnum -u https://'+ip+'/FUZZ')
-        os.system('whatweb http://'+ip+ '>> httpsEnum')
+        os.system('whatweb https://'+ip+ '>> httpsEnum')
 
     os.system('cd ..')
     print("\nFIN!")
@@ -37,7 +37,7 @@ if sys.argv[1]=='help':
     print("\nEste archivo debe ser ejecutado como root")
     print("\nEl primer parámetro debe ser el nombre del CTF")
     print("\nEl segundo parámetro debe ser el IP de la maquina victima")
-elif sys.argv[1]:
+elif sys.argv[1] and sys.argv[2]:
     dir()
 else:
     print("\nAlgo no ha salido bien")
--- recon.sh
+++ recon.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-ip=$1
+name=$1
+ip=$2
 
 dir() {
+    mkdir "$name" && cd "$name"
     mkdir nmap && mkdir exploits && mkdir doc && mkdir enum
     scanyenum
 }
 
 scanyenum() {
     cd nmap
-    sudo nmap -sS -n -Pn -min-rate 5000 -p- --open -oX portscan "$ip"
+    sudo nmap -sS -n -Pn -min-rate 5000 -p- --open -oG portscan "$ip"
     openPorts=$(grep -oP '\d+(?=/open/tcp)' portscan | tr '\n' ',' | sed 's/,$//')
-    sudo nmap -sV -sC -oN versionPorts -p "$openPorts" "$ip"
+    sudo nmap -sV -sC -oN versionPorts -p"$openPorts" "$ip"
 
     cd enum
     if [[ $openPorts == *'80'* ]]; then
@@ -19,7 +21,7 @@ scanyenum() {
         whatweb "http://$ip" >> httpEnum
     elif [[ $openPorts == *'443'* ]]; then
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpsEnum -u "https://$ip/FUZZ"
-        whatweb "http://$ip" >> httpsEnum
+        whatweb "https://$ip" >> httpsEnum
     fi
 
     cd ..
@@ -30,7 +32,7 @@ if [[ $1 == 'help' ]]; then
     echo -e "\nEste archivo debe ser ejecutado como root"
     echo -e "\nEl primer parámetro debe ser el nombre del CTF"
     echo -e "\nEl segundo parámetro debe ser el IP de la maquina victima"
-elif [[ $1 ]]; then
+elif [[ $1 && $2 ]]; then
     dir
 else
     echo -e "\nAlgo no ha salido bien"
