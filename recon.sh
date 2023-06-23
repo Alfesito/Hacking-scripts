80795af12b896b02879aa57d8cebf7f47bfb0058
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-06-23 10:23:50 +0200

    recon.sh

--- recon.sh
+++ recon.sh
@@ -12,17 +12,36 @@ dir() {
 }
 
 recon() {
-    echo -e "FASE DE RECONOCIMIENTO..."
+    echo -e "\n[+] FASE DE RECONOCIMIENTO..."
     cd recon
-    output1=$(ping -c 1 "$ip")
-    echo "$output1" > basicRecon
+    osSys
     output2=$(dnslookup "$ip")
     echo "$output2" >> basicRecon
     cd ..
 }
 
+osSys(){
+    echo -e "\n Reconocimiento de SO objetivo"
+    ping_output=$(ping -c 1 "$ip")  # Ejecuta el comando ping y almacena la salida en una variable
+    echo "$ping_output" > basicRecon
+    ttl_line=$(echo "$ping_output" | grep "ttl=")  # Obtiene la línea que contiene el valor TTL
+    ttl=$(echo "$ttl_line" | awk -F "ttl=" '{print $2}' | awk '{print $1}')  # Extrae el valor TTL de la línea
+
+    if [ -n "$ttl" ]; then
+        if [ "$ttl" -eq 64 ]; then
+            echo "\nProbablemente estás haciendo ping a un sistema Linux o macOS."
+        elif [ "$ttl" -eq 128 ]; then
+            echo "\nProbablemente estás haciendo ping a un sistema Windows."
+        else
+            echo "\nNo se puede determinar con certeza el sistema operativo basado en el TTL."
+        fi
+    else
+        echo "\nNo se encontró el valor TTL en la salida de ping."
+    fi
+}
+
 scanyenum() {
-    echo -e "FASE DE ESCANEO Y ENUMERACIÓN..."
+    echo -e "\n[+] FASE DE ESCANEO Y ENUMERACIÓN..."
     cd scan
     sudo nmap -sS -n -Pn -min-rate 5000 -p- --open -oG portscan "$ip"
     openPorts=$(grep -oP '\d+(?=/open/tcp)' portscan | tr '\n' ',' | sed 's/,$//')
@@ -30,39 +49,39 @@ scanyenum() {
 
     cd ../enum
     if [[ $openPorts == *'80'* ]]; then
-        echo -e "   >> HTTP"
+        echo -e "\n   >> HTTP"
         output1=$(whatweb "http://$ip")
         echo "$output1" > httpEnum
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpEnum -u "http://$ip/FUZZ"
         continue
     elif [[ $openPorts == *'443'* ]]; then
-        echo -e "   >> HTTPS"
+        echo -e "\n   >> HTTPS"
         output2=$(whatweb "https://$ip")
         echo "$output2" > httpsEnum
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpsEnum -u "https://$ip/FUZZ"
         continue
     elif [[ $openPorts == *'21'* ]]; then
-        echo -e "   >> FTP"
+        echo -e "\n   >> FTP"
         nmap  -n -Pn -oN ftpScript --script ftp-* -p 21 "$ip"
         continue
     elif [[ $openPorts == *'22'* ]]; then
-        echo -e "   >> SSH"
+        echo -e "\n   >> SSH"
         nmap  -n -Pn -oN sshScript --script ssh-* -p 22 "$ip"
         continue
     elif [[ $openPorts == *'23'* ]]; then
-        echo -e "   >> TelNet"
+        echo -e "\n   >> TelNet"
         nmap -n -Pn -oN telnetScript --script "*telnet* and safe" -p 23 "$ip"
         continue
     elif [[ $openPorts == *'25'* || $openPorts == *'465'* || $openPorts == *'587'* ]]; then
-        echo -e "   >> SMTP"
+        echo -e "\n   >> SMTP"
         nmap -p25,465,587 -oN smtpScript --script smtp-* "$ip"
         continue
     elif [[ $openPorts == *'53'* ]]; then
-        echo -e "   >> DNS"
+        echo -e "\n   >> DNS"
         nmap -n -oN dnsScript --script "(default and *dns*) or fcrdns or dns-srv-enum or dns-random-txid or dns-random-srcport" "$ip"
         continue
     elif [[ $openPorts == *'137'* || $openPorts == *'138'* || $openPorts == *'139'* ]]; then
-        echo -e "   >> NetBios"
+        echo -e "\n   >> NetBios"
         nmblookup -A "$ip"
         nbtscan "$ip"/30
         sudo nmap -sU -sV -T4 -oN netbiosScript --script nbstat.nse -p137 -Pn -n "$ip"
