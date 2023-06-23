b003512ee3d16318ddfa88f5bad67cdc1d126822
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-06-23 10:01:19 +0200

    recon.sh

--- recon.sh
+++ recon.sh
@@ -5,11 +5,24 @@ ip=$2
 
 dir() {
     mkdir "$name" && cd "$name"
-    mkdir scan && mkdir exploits && mkdir doc && mkdir enum
-    scanyenum
+    mkdir recon && mkdir scan && mkdir exploits && mkdir doc && mkdir enum
+    if [[ $2 ]]; then
+        recon
+        scanyenum
+}
+
+recon() {
+    echo -e "FASE DE RECONOCIMIENTO..."
+    cd recon
+    output1=$(ping -c 1 "$ip")
+    echo "$output1" > basicRecon
+    output2=$(dnslookup "$ip")
+    echo "$output2" >> basicRecon
+    cd ..
 }
 
 scanyenum() {
+    echo -e "FASE DE ESCANEO Y ENUMERACIÓN..."
     cd scan
     sudo nmap -sS -n -Pn -min-rate 5000 -p- --open -oG portscan "$ip"
     openPorts=$(grep -oP '\d+(?=/open/tcp)' portscan | tr '\n' ',' | sed 's/,$//')
@@ -17,39 +30,39 @@ scanyenum() {
 
     cd ../enum
     if [[ $openPorts == *'80'* ]]; then
-        echo -e "\HTTP"
-        whatweb "http://$ip"
+        echo -e "   >> HTTP"
+        output1=$(whatweb "http://$ip")
+        echo "$output1" > httpEnum
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpEnum -u "http://$ip/FUZZ"
-        whatweb "http://$ip" >> httpEnum
         continue
     elif [[ $openPorts == *'443'* ]]; then
-        echo -e "\HTTPS"
-        whatweb "https://$ip"
+        echo -e "   >> HTTPS"
+        output2=$(whatweb "https://$ip")
+        echo "$output2" > httpsEnum
         ffuf -c -w /usr/share/wordlists/dirb/big.txt -o httpsEnum -u "https://$ip/FUZZ"
-        whatweb "https://$ip" >> httpsEnum
         continue
     elif [[ $openPorts == *'21'* ]]; then
-        echo -e "\FTP"
+        echo -e "   >> FTP"
         nmap  -n -Pn -oN ftpScript --script ftp-* -p 21 "$ip"
         continue
     elif [[ $openPorts == *'22'* ]]; then
-        echo -e "\SSH"
+        echo -e "   >> SSH"
         nmap  -n -Pn -oN sshScript --script ssh-* -p 22 "$ip"
         continue
     elif [[ $openPorts == *'23'* ]]; then
-        echo -e "\TelNet!"
+        echo -e "   >> TelNet"
         nmap -n -Pn -oN telnetScript --script "*telnet* and safe" -p 23 "$ip"
         continue
     elif [[ $openPorts == *'25'* || $openPorts == *'465'* || $openPorts == *'587'* ]]; then
-        echo -e "\SMTP"
+        echo -e "   >> SMTP"
         nmap -p25,465,587 -oN smtpScript --script smtp-* "$ip"
         continue
     elif [[ $openPorts == *'53'* ]]; then
-        echo -e "\DNS"
+        echo -e "   >> DNS"
         nmap -n -oN dnsScript --script "(default and *dns*) or fcrdns or dns-srv-enum or dns-random-txid or dns-random-srcport" "$ip"
         continue
     elif [[ $openPorts == *'137'* || $openPorts == *'138'* || $openPorts == *'139'* ]]; then
-        echo -e "\NetBios"
+        echo -e "   >> NetBios"
         nmblookup -A "$ip"
         nbtscan "$ip"/30
         sudo nmap -sU -sV -T4 -oN netbiosScript --script nbstat.nse -p137 -Pn -n "$ip"
@@ -57,15 +70,16 @@ scanyenum() {
     fi
 
     cd ..
-    echo -e "\nFIN!!"
+    echo -e "\nSe ha terminado el reconocimiento:) Buena suerte para encontrar vuln!!"
 }
 
 if [[ $1 == 'help' ]]; then
     echo -e "\nEste archivo debe ser ejecutado como root"
     echo -e "\nEl primer parámetro debe ser el nombre del CTF"
     echo -e "\nEl segundo parámetro debe ser el IP de la maquina victima"
-elif [[ $1 && $2 ]]; then
+elif [[ $1 ]]; then
     dir
+
 else
     echo -e "\nAlgo no ha salido bien"
 fi
